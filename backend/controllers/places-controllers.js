097bee2141c9c3ace6b0dfce0dddf785f7d7a900
SadracTijerina <reavity3@gmail.com>
2022-03-07 22:59:41 -0600

    add update place using mongoose and mongodb

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -110,25 +110,43 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid inputs passed, please check your data.", 422);
   }
 
-  const placeId = req.params.pid;
-
   const { title, description } = req.body;
 
-  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
-  updatedPlace.title = title;
-  updatedPlace.description = description;
+  const placeId = req.params.pid;
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
+  let place;
 
-  res.status(200).json({ place: updatedPlace });
+  try {
+    place = await Place.findById(placeId);
+  } catch (e) {
+    const error = new HttpError(
+      "Something went wrong could not update place.",
+      500
+    );
+    return next(error);
+  }
+
+  place.title = title;
+  place.description = description;
+
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not update place",
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res, next) => {
